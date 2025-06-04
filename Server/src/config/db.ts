d9818bbf6b7988bc8bd6931af8e70fa62e99a1b2
erick949 <73764245+erick949@users.noreply.github.com>
2025-06-03 22:34:47 -0600

    Update db.ts

--- Server/src/config/db.ts
+++ Server/src/config/db.ts
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-
 const db = new Sequelize(process.env.DB_URL!, {
     models: [__dirname + "/../models/**/*.ts"],
+    logging: false, 
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 export default db; 
+
 
