b743e1cb46aea4d5ba88441a28b0b4baa8493219
erick949 <73764245+erick949@users.noreply.github.com>
2025-06-03 23:13:25 -0600

    Update index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,7 +1,7 @@
-import server from "./server";
+import server from './server';
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
