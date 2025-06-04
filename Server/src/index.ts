b39e96ab9fa5aeee529d9eebc3833ecb490f80d6
erick949 <73764245+erick949@users.noreply.github.com>
2025-06-03 23:08:27 -0600

    Update index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,6 +1,7 @@
 import server from "./server";
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    console.log('http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
